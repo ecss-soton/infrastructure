89f6c61c66bb4c5b3f510d873e2581f47cdaebe9
Euan Caskie <56805259+Ortovoxx@users.noreply.github.com>
2024-03-05 15:59:29 +0000

    Create update.sh

---
+++ scripts/update.sh
@@ -0,0 +1,11 @@
+#!/usr/bin bash
+
+# Pull the latest changes from the remote repository
+git pull
+
+# Pull the latest images from the Docker Hub
+docker compose pull
+
+# Restart the containers
+docker compose down
+docker compose up -d
