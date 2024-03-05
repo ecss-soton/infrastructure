e09d520e40312d1e38a6d328ed8bc18793a53fbf
Euan Caskie <56805259+Ortovoxx@users.noreply.github.com>
2024-03-05 16:35:56 +0000

    Remove space

--- scripts/backup-env.sh
+++ scripts/backup-env.sh
@@ -3,7 +3,7 @@
 mkdir -p backups
 
 # Get the current date in the format YYYY-MM-DD HH:MM:SS
-date=$(date +"%Y-%m-%d %H:%M:%S")
+date=$(date +"%Y-%m-%dT%H:%M:%S")
 
 # Create a backup of the environment files
 tar --exclude='*.example' -zcvf "backups/backup-env-${date}".tar.gz env
