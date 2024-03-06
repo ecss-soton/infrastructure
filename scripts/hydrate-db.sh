79778005375d036fa7d066e6bc6589a17a04882b
Euan Caskie <56805259+Ortovoxx@users.noreply.github.com>
2024-03-06 00:48:04 +0000

    Copy media

--- scripts/hydrate-db.sh
+++ scripts/hydrate-db.sh
@@ -5,6 +5,7 @@ git clone https://github.com/ecss-soton/new-web.git
 
 # Copy the data from the cloned repository to the database container
 docker cp new-web/data/dump/ecss-website-cms/. db_mongo:/ecss-website-cms
+docker cp new-web/media/. db_mongo:/ecss-media
 
 # Remove the cloned repository
 rm -rf new-web
