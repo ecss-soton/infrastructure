9f2cc184a8727b3261301132f51b1bcbadea3501
Euan Caskie <56805259+Ortovoxx@users.noreply.github.com>
2024-03-20 18:30:48 +0000

    Add restore command

--- scripts/restore-db.sh
+++ scripts/restore-db.sh
@@ -6,10 +6,19 @@ if [[ $# -eq 0 ]] ; then
 fi
 
 # Create a directory for the backup to go
-mkdir -p backus/db/tmp
+mkdir -p backups/db/tmp
 
 # Extract the backup
-tar -xvzf $1 -C backus/db/tmp
+tar -xvzf $1 -C backups/db/tmp
+
+# Copy the backup from docker to the host
+docker cp backups/db/tmp/backups/db/ecss-website-cms db_mongo:/ecss-website-cms
+
+# Restore the database (https://www.mongodb.com/docs/database-tools/mongorestore/)
+docker exec db_mongo mongorestore --uri="mongodb://root:example@db_mongo:27017/ecss-website-cms?authSource=admin" --db=ecss-website-cms /ecss-website-cms
+
+# Remove the backup from the docker container
+docker exec db_mongo rm -rf /ecss-website-cms
 
 # Remove the backup folder from the host
 rm -rf backups/db/tmp
