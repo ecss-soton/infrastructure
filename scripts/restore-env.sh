f709391fe3279ee72084199cc7c5b9e606f8d98d
Euan Caskie <56805259+Ortovoxx@users.noreply.github.com>
2024-03-20 17:48:01 +0000

    Update restore-env.sh

--- scripts/restore-env.sh
+++ scripts/restore-env.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+if [[ $# -eq 0 ]] ; then
+    echo 'Please provide a backup file to restore from as the first argument.'
+    exit 0
+fi
 
 # Create a backup of the environment files
-tar -xvzf backup.tar.gz -C env
+tar -xvzf $1 -C env
