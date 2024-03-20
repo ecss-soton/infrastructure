3190aa4650e2d878abed1199878f86a945a5d0be
Euan Caskie <56805259+Ortovoxx@users.noreply.github.com>
2024-03-20 20:03:41 +0000

    Copy media in backup

--- scripts/backup-db.sh
+++ scripts/backup-db.sh
@@ -1,19 +1,20 @@
 #! /bin/bash
 
 # Create directory for backup to go
-mkdir -p backups/db
+mkdir -p backups/db/tmp
 
 # Create a backup of the database (https://www.mongodb.com/docs/database-tools/mongodump/)
 docker exec db_mongo mongodump --uri="mongodb://root:example@db_mongo:27017/ecss-website-cms?authSource=admin" --db=ecss-website-cms --out=/
 
 # Copy the backup from docker to the host
-docker cp db_mongo:/ecss-website-cms/. backups/db/ecss-website-cms
+docker cp db_mongo:/ecss-website-cms backups/db/tmp/ecss-website-cms
+docker cp web_main:/home/node/app/media backups/db/tmp/media
 
 # Get the current date in the format YYYY-MM-DD HHMMSS
 date=$(date +"%Y-%m-%dT%H%M%S")
 
 # Create a tarball of the backup
-tar -zcf "backups/db/backup-db-${date}.tar.gz" backups/db/ecss-website-cms
+tar -zcf "backups/db/backup-db-${date}.tar.gz" backups/db/tmp
 
 # Remove the backup folder on the host
 rm -rf backups/db/ecss-website-cms
