e90189d543affdf842f48f717df0a7137d18ef6e
casperUoS <casperdancy@gmail.com>
2024-10-03 14:50:59 +0100

    new infrastructure to support nginx

--- scripts/restore-db.sh
+++ scripts/restore-db.sh
@@ -22,16 +22,16 @@ mkdir -p backups/db/tmp
 tar -xvzf $1 -C backups/db/tmp
 
 # Remove MacOS specific meta-files from extract
-rm backups/db/tmp/._*
-rm backups/db/tmp/ecss-website-cms/._*
-rm backups/db/tmp/media/._*
+# rm backups/db/tmp/._*
+# rm backups/db/tmp/ecss-website-cms/._*
+# rm backups/db/tmp/media/._*
 
 # Copy the backup from docker to the host
 docker cp backups/db/tmp/ecss-website-cms/. db_mongo:/ecss-website-cms
 docker cp backups/db/tmp/media/. web_main:/home/node/app/media
 
 # Restore the database (https://www.mongodb.com/docs/database-tools/mongorestore/)
-docker exec db_mongo mongorestore --uri="$uri" --db=ecss-website-cms /ecss-website-cms
+docker exec db_mongo mongorestore --uri="$uri" --drop --db=ecss-website-cms /ecss-website-cms
 
 # Remove the backup from the docker container
 docker exec db_mongo rm -rf /ecss-website-cms
