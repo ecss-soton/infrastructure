86addacf9e40238be518c3a374e70788bdb7e381
Indigo Nolan <dev@indigonolan.com>
2026-05-04 22:48:38 +0100

    goaccess improve

--- scripts/goaccess.sh
+++ scripts/goaccess.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-awk -F\" '$2 ~ /^GET [^ ]+ HTTP/ {
+awk -F\" '{
+  if ($2 !~ /^GET /) next
   url = $2
-  sub(/^GET /,"",url)
-  sub(/ HTTP/,"",url)
+  sub(/^GET /, "", url)
+  sub(/ HTTP\/[0-9.]+/, "", url)
+
+  # Drop everything that isn’t a real page view
   if (url ~ /\.(js|css|png|jpg|jpeg|gif|svg|ico|woff2?|webp|avif|json|xml|txt|map)(\?|$)/) next
   if (url ~ /^\/_next\//) next
   if (url ~ /^\/api\//) next
   if (url ~ /^\/service\//) next
   if (url ~ /^\/favicon\.ico$/) next
+  if (url ~ /^\/webpack-hmr/) next
+
   print
-}' /var/log/nginx/society-pages.log \
-| /usr/bin/goaccess --log-format=COMBINED --ignore-crawlers \
+}' /var/log/nginx/society-pages.log | /usr/bin/goaccess --log-format=COMBINED --ignore-crawlers \
     --db-path=/srv/goaccess/db/ --persist --restore \
     --output=/var/www/html/stats.html
